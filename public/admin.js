e0378841f6292071c211b518397cbc35e4da0a32
twedty <jamestwedt@gmail.com>
2024-03-18 21:06:46 -0400

    worked in the class and finished the assignment

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,37 @@
+async function main() {
+    const response = await fetch('http://localhost:3001/listBooks')
+    const data = await response.json()
 
-// Your Code Here
+    data.forEach(book => renderInput(book))
+}
+
+function renderInput(book) {
+    const root = document.getElementById('root')
+
+    const li = document.createElement('li')
+    li.textContent = book.title
+
+    const input = document.createElement('input')
+    input.value = book.quantity
+
+    const saveBtn = document.createElement('button')
+    saveBtn.textContent = 'Save'
+
+    saveBtn.addEventListener('click', async function() {
+        await fetch('http://localhost:3001/updateBook', {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: book.id,
+                quantity: input.value
+            })
+        })
+    })
+
+    li.append(input, saveBtn)
+    root.append(li)
+}
+
+main()
